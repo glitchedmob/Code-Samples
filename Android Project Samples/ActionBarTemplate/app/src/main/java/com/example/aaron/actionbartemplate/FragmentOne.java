3d84bb7c4db5a8826a68be1e28e9a8718314a59d
Levi Zitting <levizitting5663@gmail.com>
2016-12-27 20:33:35 -0600

    Adding in all of the files

---
+++ Android Project Samples/ActionBarTemplate/app/src/main/java/com/example/aaron/actionbartemplate/FragmentOne.java
@@ -0,0 +1,40 @@
+package com.example.aaron.actionbartemplate;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentOne extends Fragment {
+
+    public static final String ARG_PAGE = "ARG_PAGE";
+
+    private int mPage;
+
+    public static FragmentOne newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        FragmentOne fragment = new FragmentOne();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + mPage);
+        return view;
+    }
+
+
+}
